70d22909f798cbf9d595f97df9825a37f55363b2
Oliver <oliverwang008@gmail.com>
2023-09-20 17:48:02 +1000

    second commit

--- class/cursor.js
+++ class/cursor.js
@@ -16,26 +16,48 @@ class Cursor {
 
   resetBackgroundColor() {
     Screen.setBackgroundColor(this.row, this.col, this.gridColor);
+    Screen.render();
   }
 
   setBackgroundColor() {
     Screen.setBackgroundColor(this.row, this.col, this.cursorColor);
+    Screen.render();
   }
 
   up() {
     // Move cursor up
+    this.resetBackgroundColor();
+    if (this.row > 0) {
+      this.row -= 1;
+    }
+    this.setBackgroundColor();
   }
 
   down() {
     // Move cursor down
+    this.resetBackgroundColor();
+    if (this.row < 2) {
+      this.row += 1;
+    }
+    this.setBackgroundColor();
   }
 
   left() {
     // Move cursor left
+    this.resetBackgroundColor();
+    if (this.col > 0) {
+      this.col -= 1;
+    }
+    this.setBackgroundColor();
   }
 
   right() {
     // Move cursor right
+    this.resetBackgroundColor();
+    if (this.col < 2) {
+      this.col += 1;
+    }
+    this.setBackgroundColor();
   }
 
 }
--- class/ttt.js
+++ class/ttt.js
@@ -12,28 +12,94 @@ class TTT {
                  [' ',' ',' ']]
 
     this.cursor = new Cursor(3, 3);
+    this.preMove = 'O';
 
     // Initialize a 3x3 tic-tac-toe grid
     Screen.initialize(3, 3);
     Screen.setGridlines(true);
+    Screen.setBackgroundColor(0, 0, 'yellow');
 
-    // Replace this with real commands
-    Screen.addCommand('t', 'test command (remove)', TTT.testCommand);
+
+    Screen.addCommand('up', 'cursor up', this.cursor.up.bind(this.cursor));
+    Screen.addCommand('down', 'cursor down', this.cursor.down.bind(this.cursor));
+    Screen.addCommand('left', 'cursor left', this.cursor.left.bind(this.cursor));
+    Screen.addCommand('right', 'cursor right', this.cursor.right.bind(this.cursor));
+    Screen.addCommand('return', 'place move', this.placeMove.bind(this));
 
     Screen.render();
   }
 
-  // Remove this
-  static testCommand() {
-    console.log("TEST COMMAND");
-  }
+  placeMove(){
+    let r = this.cursor.row;
+    let c = this.cursor.col;
+    if (this.grid[r][c] === ' ') {
+      if (this.preMove === 'O') {
+        this.grid[r][c] = 'X';
+        this.preMove = 'X';
+        Screen.setGrid(r, c, 'X');
+        Screen.render();
+      } else {
+        this.grid[r][c] = 'O';
+        this.preMove = 'O'
+        Screen.setGrid(r, c, 'O');
+        Screen.render();
+      }
+    }
+    let winner = TTT.checkWin(this.grid);
+    if (winner) {
+      TTT.endGame(winner);
+    }
+    
+  };
+
+
 
   static checkWin(grid) {
-
     // Return 'X' if player X wins
     // Return 'O' if player O wins
     // Return 'T' if the game is a tie
     // Return false if the game has not ended
+    for (let r = 0; r < 3; r++) {
+      if (grid[r][0] === 'X' && grid[r][1] === 'X' && grid[r][2] === 'X') {
+        return 'X';
+      }
+      if (grid[r][0] === 'O' && grid[r][1] === 'O' && grid[r][2] === 'O') {
+        return 'O';
+      }
+    }
+    for (let c = 0; c < 3; c++) {
+      if (grid[0][c] === 'X' && grid[1][c] === 'X' && grid[2][c] === 'X') {
+        return 'X';
+      }
+      if (grid[0][c] === 'O' && grid[1][c] === 'O' && grid[2][c] === 'O') {
+        return 'O';
+      }
+    }
+    if (grid[0][0] === 'X' && grid[1][1] === 'X' && grid[2][2] === 'X') {
+      return 'X';
+    }
+    if (grid[0][0] === 'O' && grid[1][1] === 'O' && grid[2][2] === 'O') {
+      return 'O';
+    }
+    if (grid[0][2] === 'X' && grid[1][1] === 'X' && grid[2][0] === 'X') {
+      return 'X';
+    }
+    if (grid[0][2] === 'O' && grid[1][1] === 'O' && grid[2][0] === 'O') {
+      return 'O';
+    }
+    let tie = true;
+    for (let r = 0; r < 3; r++) {
+      for (let c = 0; c < 3; c++) {
+        if (grid[r][c] === ' ') {
+          tie = false;
+        }
+      }
+    }
+    if (tie) {
+      return 'T';
+    } else {
+      return false;
+    }
 
   }
 
@@ -48,6 +114,7 @@ class TTT {
     Screen.render();
     Screen.quit();
   }
+
 
 }
 
